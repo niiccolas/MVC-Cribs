35c58fa57cacf59e9144bd2ef49d7a9612b3e752
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-08-29 14:32:31 +0200

    Add HouseBuilder class

--- src/Controller/HouseBuilder.java
+++ src/Controller/HouseBuilder.java
@@ -1,4 +1,29 @@
 package Controller;
 
+import java.util.Scanner;
+
+import Model.House;
+import Model.Room;
+import Vue.UITools;
+
 public class HouseBuilder {
+  public static void addRoom() {
+
+  }
+
+  public static House build() {
+    Scanner input = new Scanner(System.in);
+    UITools.prompt("Enter a name for your house: ");
+
+    String houseName = input.nextLine();
+    System.out.println("*** The House of " + houseName + " ***");
+
+    String[] entranceFurniture = {"welcome rug"};
+    Room entrance = new Room("Entrance", 5, entranceFurniture);
+
+    Room[] houseRooms = {entrance};
+    House newHouse = new House(houseName, houseRooms);
+
+    return newHouse;
+  }
 }
